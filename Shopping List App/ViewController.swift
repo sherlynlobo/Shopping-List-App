c030bf23f3414580844e04b20676590ddc7c99db
Sherlyn Lobo <sherlynlobo@macbook-pro-2.local>
2018-10-18 21:39:57 -0400

    Added functionality for cancel button

--- Shopping List App/ViewController.swift
+++ Shopping List App/ViewController.swift
@@ -5,6 +5,12 @@
 //  Created by Sherlyn Lobo on 2018-10-18.
 //  Copyright © 2018 Sherlyn Lobo. All rights reserved.
 //
+//  File Name       : ViewController.swift
+//  Author Name     : Sherlyn Lobo
+//  Student ID      : 301013071
+//  Date            : 18-10-2018 (DD/MM/YYYY)
+//  App Description : Shopping List
+
 
 import UIKit
 
@@ -20,10 +26,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func ItemsList(_ sender: UITextField)
-    {
+    
+       @IBOutlet var Items: [UITextField]!
+    
+    func textf(_ texf: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard CharacterSet(charactersIn: "0123456789").isSuperset(of: CharacterSet(charactersIn: string)) else {
+            return false
+        }
+        return true
+    }
+    
+
+ 
+    //Reset Everything
+    @IBAction func CancelButton(_ sender: UIButton) {
+        
+        Items.forEach ({ $0.text=""})
+        }
     
         
+    
+    
+    @IBAction func SaveButton(_ sender: UIButton) {
     }
     
 }
